0ea2ed33884a0a1745a5bebc6cb8e695a51501d0
ntnyq <ntnyq13@gmail.com>
2023-10-20 18:53:23 +0800

    feat: comment a message

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 'use strict'
 
 import process from 'node:process'
-import React from 'react'
+import React, { useState } from 'react'
 import open from 'open'
 import { Box, Text } from 'ink'
 import Link from 'ink-link'
 import BigText from 'ink-big-text'
 import Gradient from 'ink-gradient'
 import SelectInput from 'ink-select-input'
+import { UncontrolledTextInput } from 'ink-text-input'
 import type { ISelectItem } from './types'
-
-const handleSelect = (item: ISelectItem) => {
-  if (item.url) return open(item.url)
-  item.action?.()
-}
 
 const createItems = (items: Omit<ISelectItem, 'value'>[]) =>
   items.map(item => ({
@@ -21,72 +17,106 @@ const createItems = (items: Omit<ISelectItem, 'value'>[]) =>
     value: item.label,
   }))
 
-const items: ISelectItem[] = createItems([
-  {
-    label: 'Website',
-    url: 'https://ntnyq.com',
-  },
-  {
-    label: 'Twitter',
-    url: 'https://twitter.com/ntnyq',
-  },
-  {
-    label: 'GitHub',
-    url: 'https://github.com/ntnyq',
-  },
-  {
-    label: 'Bilibili',
-    url: 'https://space.bilibili.com/40199161',
-  },
-  {
-    label: 'Juejin',
-    url: 'https://juejin.cn/user/1591748566717591',
-  },
-  {
-    label: 'Segment Fault',
-    url: 'https://segmentfault.com/u/ntnyq',
-  },
-  {
-    label: 'Quit',
-    action() {
-      process.exit()
-    },
-  },
-])
-
 export interface Props {
   name: string
 }
 
-export const App: React.FC<Props> = ({ name }) => (
-  <Box flexDirection='column'>
-    <Gradient name='rainbow'>
-      <BigText text={name} />
-    </Gradient>
-    <Box marginBottom={1}>
-      <Link
-        url={`https://${name}.com`}
-        fallback
-      >
-        Hello, this is <Text color='cyan'>{name}</Text>, an FE developer.
-      </Link>
-    </Box>
-    <SelectInput<string>
-      items={items}
-      onSelect={handleSelect}
-    />
+export const App: React.FC<Props> = ({ name }) => {
+  const [isComment, setIsComment] = useState(false)
+
+  const items: ISelectItem[] = createItems([
+    {
+      label: 'Website',
+      url: 'https://ntnyq.com',
+    },
+    {
+      label: 'Twitter',
+      url: 'https://twitter.com/ntnyq',
+    },
+    {
+      label: 'GitHub',
+      url: 'https://github.com/ntnyq',
+    },
+    {
+      label: 'Bilibili',
+      url: 'https://space.bilibili.com/40199161',
+    },
+    {
+      label: 'Juejin',
+      url: 'https://juejin.cn/user/1591748566717591',
+    },
+    {
+      label: 'Segment Fault',
+      url: 'https://segmentfault.com/u/ntnyq',
+    },
+    {
+      label: 'Leave a message',
+      action() {
+        setIsComment(true)
+      },
+    },
+    {
+      label: 'Quit',
+      action() {
+        process.exit()
+      },
+    },
+  ])
+
+  const handleSelect = (item: ISelectItem) => {
+    if (item.url) return open(item.url)
+    item.action?.()
+  }
+  const handleSubmit = (message: string) => {
+    if (message.length) {
+      open(`https://github.com/ntnyq/ntnyq/issues/new?title=${encodeURIComponent(message)}`)
+    }
+    return setIsComment(false)
+  }
+
+  return isComment ? (
     <Box
-      justifyContent='center'
-      width='40%'
-      borderStyle='classic'
-      borderColor='#0ff'
-      marginTop={1}
-      paddingY={1}
-      paddingX={1}
+      flexDirection='column'
+      marginY={1}
     >
-      <Text>
-        Copyright &copy; <Text color='green'>{name}</Text>
-      </Text>
+      <Box marginBottom={1}>
+        <Text>Enter to submit a message or go back:</Text>
+      </Box>
+      <UncontrolledTextInput
+        placeholder='A message to me'
+        onSubmit={handleSubmit}
+      />
     </Box>
-  </Box>
-)
+  ) : (
+    <Box flexDirection='column'>
+      <Gradient name='rainbow'>
+        <BigText text={name} />
+      </Gradient>
+      <Box marginBottom={1}>
+        <Link
+          url={`https://${name}.com`}
+          fallback
+        >
+          Hello, this is <Text color='cyan'>{name}</Text>, an FE developer.
+        </Link>
+      </Box>
+      <SelectInput
+        items={items}
+        onSelect={handleSelect}
+      />
+      <Box
+        justifyContent='center'
+        width='40%'
+        borderStyle='classic'
+        borderColor='#0ff'
+        marginTop={1}
+        paddingY={1}
+        paddingX={1}
+      >
+        <Text>
+          Copyright &copy; <Text color='green'>{name}</Text>
+        </Text>
+      </Box>
+    </Box>
+  )
+}
